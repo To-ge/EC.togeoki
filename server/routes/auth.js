22dd8dd606e4f97d0710dbb0bb5a993b2dd8624b
to-ge <tougei324@gmail.com>
2022-09-19 19:18:14 +0900

    folder add

---
+++ server/routes/auth.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const crypto = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+// 登録
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: crypto.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// ログイン
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials!");
+
+    const decryptedPassword = crypto.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = decryptedPassword.toString(crypto.enc.Utf8);
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials!");
+
+    const accesstoken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accesstoken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
